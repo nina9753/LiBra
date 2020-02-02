70f7cecd07e5782bf2cfba2cf258b9d85f1daea4
Zakia Hossain <zakia.hossain@temple.edu>
2020-02-02 04:03:10 -0500

    update

--- app/src/main/java/com/example/libra_project/InfoPage2.java
+++ app/src/main/java/com/example/libra_project/InfoPage2.java
@@ -1,6 +1,9 @@
 package com.example.libra_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,14 @@ public class InfoPage2 extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pageinfo2);
+        Button submitB = findViewById(R.id.submitB);
+
+        submitB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InfoPage2.this, clarifyPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/libra_project/clarifyPage.java
+++ app/src/main/java/com/example/libra_project/clarifyPage.java
@@ -1,6 +1,9 @@
 package com.example.libra_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,14 @@ public class clarifyPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.clarify_main);
+        Button start = findViewById(R.id.start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(clarifyPage.this, timerFront.class);
+                startActivity(intent);
+            }
+        });
     }
 }
